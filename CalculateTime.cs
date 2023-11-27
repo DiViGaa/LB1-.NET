bfc26957f75ddd14e1b5e200329480c5ab381633
DiViGaa <131615743+DiViGaa@users.noreply.github.com>
2023-11-27 18:48:45 +0200

    Add files via upload

---
+++ CalculateTime.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate
+{
+    public class CalculateTime
+    {
+        public void CalculateAndPrintSpeedup(long singleThreadedTime, long multiThreadedTime)
+        {
+            double speedup = (double)singleThreadedTime / multiThreadedTime;
+            Console.WriteLine($"\nПрискорення: {speedup:F2} раза");
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using PrintClass;
+using Shift;
+using Calculate;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int size = 100000;
+        int[] vector = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            vector.SetValue(i, i);
+        }
+
+        int shiftPositions = 50000;
+        CalculateTime calculateTime = new CalculateTime();
+
+        var singleThreadedStopwatch = new Stopwatch();
+        singleThreadedStopwatch.Start();
+        VectorShift.ShiftLeft(vector, shiftPositions);
+        singleThreadedStopwatch.Stop();
+        Console.WriteLine($"\nРезультат 1 потока: {singleThreadedStopwatch.ElapsedTicks} мс");
+
+        for (int i = 2; i < 5; i++)
+        {
+            var threadedStopwatch = new Stopwatch();
+            threadedStopwatch.Start();
+            VectorShift.ShiftLeftParallel(vector.ToArray(), shiftPositions, i);
+            threadedStopwatch.Stop();
+            Console.WriteLine($"\nРезультат {i} потокiв: {threadedStopwatch.ElapsedTicks} мс");
+            calculateTime.CalculateAndPrintSpeedup(singleThreadedStopwatch.ElapsedTicks, threadedStopwatch.ElapsedTicks);
+        }
+
+
+    }
+}
