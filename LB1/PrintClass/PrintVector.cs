64264462529a9c155037f6affa9202471e2b2806
DiViGaa <131615743+DiViGaa@users.noreply.github.com>
2023-11-27 18:52:21 +0200

    Add files via upload

---
+++ LB1/PrintClass/PrintVector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintClass
+{
+    public class PrintArr
+    {
+        public static void PrintVector(int[] array)
+        {
+            foreach (int element in array)
+            {
+                Console.Write($"{element} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ LB1/Shift/ShiftVector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shift
+{
+    public class VectorShift
+    {
+        public static void ShiftLeft(int[] array, int positions)
+        {
+            int length = array.Length;
+            int[] result = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                int newPosition = (i - positions + length) % length;
+                result[newPosition] = array[i];
+            }
+        }
+
+        public static void ShiftLeftParallel(int[] array, int positions, int numThreads)
+        {
+            int length = array.Length;
+            int[] result = new int[length];
+
+            Parallel.For(numThreads, length, i =>
+            {
+                int newPosition = (i - positions + length) % length;
+                result[newPosition] = array[i];
+            });
+        }
+    }
+}
